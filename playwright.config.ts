eee6a37959fd2ae61570bcd22095026fead50cad
TonyGorman <32516413+TonyGorman@users.noreply.github.com>
2026-05-12 14:28:11 +0100

    removed platform from snapshot file names

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
+  snapshotPathTemplate: '{testDir}/{testFilePath}-snapshots/{arg}-{projectName}{ext}',
   testDir: './tests/e2e',
   fullyParallel: true,
   retries: process.env.CI ? 2 : 0,
