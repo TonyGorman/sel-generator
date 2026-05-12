819ee6eb788f21129e00dba617dff1568b6fcbd4
TonyGorman <32516413+TonyGorman@users.noreply.github.com>
2026-05-12 10:18:07 +0100

    deletes these redundant files to stop them re-appearing

--- src/models/IBarcodeGenerator.ts
+++
@@ -1,9 +0,0 @@
-import { ILabelConfig } from './IBarcodeConfig';
-import { LabelPrintMode } from './ILabelLayoutStrategy';
-
-export interface ILabelGenerator {
-  aisles: string[];
-  type?: string;
-  config: ILabelConfig;
-  layoutMode?: LabelPrintMode;
-}
